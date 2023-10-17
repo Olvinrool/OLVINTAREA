fde315e5f68398abb8d0f2563eee25c6dc0e3d90
DESKTOP-AOQ6DJH\olvin <olvinrool@gmail.com>
2023-10-16 22:40:27 -0600

    Agregar archivos de proyecto.

---
+++ OLVINTAREA/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace OLVINTAREA
+{
+    public class Empleado
+    {
+        [Required(ErrorMessage = "El campo 'Nombres' es obligatorio.")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Apellidos' es obligatorio.")]
+        public string Apellidos { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Fecha de Nacimiento' es obligatorio.")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [RegularExpression(@"^[\w\.-]+@[\w\.-]+\.\w+$", ErrorMessage = "El correo electrónico no es válido.")]
+        public string Correo { get; set; }
+    }
+}
---
+++ OLVINTAREA/Paginas/IngresarDatosPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OLVINTAREA.Paginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class IngresarDatosPage : ContentPage
+    {
+        private Empleado empleado = new Empleado();
+
+        public IngresarDatosPage()
+        {
+            InitializeComponent();
+            BindingContext = empleado;
+        }
+
+        private async void OnGuardarClicked(object sender, EventArgs e)
+        {
+            var context = new ValidationContext(empleado, null, null);
+            var results = new List<ValidationResult>();
+
+            if (Validator.TryValidateObject(empleado, context, results, true))
+            {
+                await Navigation.PushModalAsync(new ResultadoPage() { BindingContext = empleado });
+            }
+            else
+            {
+                var errors = results.Select(result => result.ErrorMessage);
+                await DisplayAlert("Error", string.Join("\n", errors), "OK");
+            }
+        }
+    }
+
+
+}
